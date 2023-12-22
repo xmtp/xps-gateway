cce87ee236d79ea88fa3f9fc1a2678c6a1adf0fc
Andrew Plaza <github@andrewplaza.dev>
2023-12-22 17:41:10 -0500

    set default level of `info`

--- xps-gateway/src/util.rs
+++ xps-gateway/src/util.rs
@@ -9,10 +9,7 @@ static INIT: Once = Once::new();
 
 pub(crate) fn init_logging() {
     let fmt = fmt::layer().compact();
-    Registry::default()
-        .with(EnvFilter::from_default_env())
-        .with(fmt)
-        .init()
+    Registry::default().with(env()).with(fmt).init()
 }
 
 #[cfg(test)]
@@ -20,9 +17,12 @@ pub(crate) fn init_logging() {
 fn __init_test_logging() {
     INIT.call_once(|| {
         let fmt = fmt::layer().compact();
-        Registry::default()
-            .with(EnvFilter::from_default_env())
-            .with(fmt)
-            .init()
+        Registry::default().with(env()).with(fmt).init()
     })
+}
+
+/// Try to get the logging environment from the `RUST_LOG` environment variable.
+/// If it is not set, use the default of `info`.
+fn env() -> EnvFilter {
+    EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"))
 }
