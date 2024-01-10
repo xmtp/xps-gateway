66ac55f158f7835ece70fe4bae98a3cb176a0f22
Tsachi Herman <tsachi@Tsachis-MacBook-Pro.local>
2024-01-10 16:58:42 -0500

    stage

--- xps-gateway/src/rpc/methods.rs
+++ xps-gateway/src/rpc/methods.rs
@@ -5,7 +5,7 @@ use super::api::*;
 use async_trait::async_trait;
 use jsonrpsee::types::ErrorObjectOwned;
 
-use crate::types::Message;
+use crate::types::{GrantInstallationResult, Message, Signature};
 
 /// Gateway Methods for XPS
 pub struct XpsMethods;
@@ -16,5 +16,24 @@ impl XpsServer for XpsMethods {
         //TODO: Stub for sendMessage, ref: [discussion](https://github.com/xmtp/xps-gateway/discussions/11)
         log::debug!("xps_sendMessage called");
         todo!();
+    }
+
+    async fn grant_installation(
+        &self,
+        _did: String,
+        _name: String,
+        _value: String,
+        _signature: Signature,
+    ) -> Result<GrantInstallationResult, ErrorObjectOwned> {
+        /*if name.len() > 32 {
+            Err(GrantInstallationResult {})
+        }*/
+
+        let result = GrantInstallationResult {
+            status: "my-status".to_string(),
+            message: "my message".to_string(),
+            transaction: "my transaction".to_string(),
+        };
+        Ok(result)
     }
 }
--- xps-gateway/src/types.rs
+++ xps-gateway/src/types.rs
@@ -15,3 +15,26 @@ pub struct Message {
     /// Signature of S
     s: Vec<u8>,
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct Signature {
+    /// Signature of V
+    #[serde(rename = "V")]
+    v: i64,
+    /// Signature of R
+    #[serde(rename = "R")]
+    r: Vec<u8>,
+    /// Signature of S
+    #[serde(rename = "S")]
+    s: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct GrantInstallationResult {
+    #[serde(rename = "status")]
+    pub status: String,
+    #[serde(rename = "message")]
+    pub message: String,
+    #[serde(rename = "tx")]
+    pub transaction: String,
+}
